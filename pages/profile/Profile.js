b2cf67b08ae40830d05ba77672f8618fcd8edee5
bx23 <bx23@gold31.cs.calvin.edu>
2021-10-21 22:37:35 -0400

    login Page1

---
+++ pages/profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { profileStyles } from './profileStyles';
+import {Login} from './Login';
+
+export default function About() {
+  return (
+    <Login/>
+  )
+}

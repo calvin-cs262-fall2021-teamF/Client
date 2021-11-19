22e51f09bf20c424d25c4225f84318b061158a40
se-624 <sv44@students.calvin.edu>
2021-11-19 00:48:01 -0500

    Added heading to history page

--- pages/history/History.js
+++ pages/history/History.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, Image, ImageBackground } from "react-native";
 import { historyStyles } from "./historyStyles";
-import { Title, Checkbox } from "react-native-paper";
+import { Title, Checkbox, Avatar } from "react-native-paper";
+import GlobalFont from "react-native-global-font";
 
 export default function HistoryScreen({ navigation }) {
+
   return (
     <View style={historyStyles.container}>
+      <Text style={historyStyles.headerText}> Brushing History </Text>
+      <Text style={historyStyles.subtitleText}> "You don't have to brush all your teeth, just the ones you want to keep" </Text>
       <Text style={historyStyles.date}>5 Nov 2021</Text>
       <View style={historyStyles.box}>
         <Title>01:30</Title>
--- pages/history/historyStyles.js
+++ pages/history/historyStyles.js
@@ -18,11 +18,42 @@ export const historyStyles = StyleSheet.create({
     paddingTop: 5,
     paddingBottom: 5,
     marginTop: 10,
+    marginBottom: 3,
     height: 60,
   },
   date: {
     fontSize: 24,
     fontWeight: "bold",
-    marginTop: 20,
+    marginTop: 3,
   },
+
+  headerText: {
+    fontSize: 25, 
+    textAlign: "center",
+    color: "black",
+    marginTop: 5,
+    fontWeight: "bold",
+  },
+
+  subtitleText: {
+    fontSize: 15, 
+    textAlign: "center",
+    color: "black",
+    marginTop: 1,
+    marginBottom: 10,
+  }, 
+
+  headerImage: {
+		width: 1500,
+		height: 1000,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+  imageContainer: {
+		flex: 1, 
+		flexDirection: 'column', 
+		justifyContent: 'center', 
+		alignItems: 'center', 
+	}
 });
